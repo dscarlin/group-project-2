1687f4fc725fc6208f463698306dea10831a6250
David Carlin <dave.carlin86@gmail.com>
2019-05-19 23:35:59 -0400

    notification file and demo

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,13 +2,17 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld :msg="msg.greeting"/>
+    <input v-model="range" type="range">
+    <div>{{range}}</div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import axios from 'axios'
+import HelloWorld from '@/components/HelloWorld.vue';
+import {notify} from '@/services/notify.js'
+import axios from 'axios';
 
 export default {
   name: 'home',
@@ -17,18 +21,28 @@ export default {
   },
   data: function() {
     return {
+      range: 0,
       msg: {
 
-      }
+      },
+      notification: "Steven Wright for the next 45 minutes"
     }
   },
   created: function() {
+    this.notifyMe();
     axios.get('api/test').then(
       (response) => {
-      console.log(response.data);
+        console.log(response.data);
       this.msg = response.data;
       }
     );
+  },
+  methods: {
+  notifyMe: function() {
+
+    notify(this.notification);
+    
+    }
   }
 }
 </script>
--- server.js
+++ server.js
@@ -4,15 +4,36 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//twilio vars
+require('dotenv').config()
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const trialNumber = process.env.TRIAL_NUMBER
+const twilio = require('twilio')(accountSid, authToken);
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/dist"));
 }
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Sample API
 app.get('/api/test', function(req, res) {
   res.json({ greeting: 'There is no topping teamPizza.slice()' });
 });
+
+app.post('/twilio',function(req,res){
+  let body = req.body.message
+  console.log('twilio post')
+  // twilio.messages.create({
+  //   body: body,
+  //   from: trialNumber,
+  //   to: '+19193682008'
+  //   }).then(message => res.json(message.sid))
+})
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
