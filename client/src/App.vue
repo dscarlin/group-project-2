b5138c8736f9697a2c16a9339fbc605ad597fb39
lgfahys <lindsey@codeasite.com>
2019-05-20 21:36:46 -0400

    added basic navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">Home</router-link> | -->
+      <!-- <router-link to="/about">About</router-link> -->
+      <navbar></navbar>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+  import navbar from './components/navbar.vue'
+  export default {
+  name: 'app',
+  components: {
+    navbar
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -17,10 +28,13 @@
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
+  .navbar {
+    background-color: whitesmoke;
+    // opacity:
+  }
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: black;
     &.router-link-exact-active {
       color: #42b983;
     }
---
+++ client/src/components/navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="http://placehold.it/150x50?text=Logo" alt>
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <router-link to="/about">About Us</router-link>
+            </li>
+            <router-link to="/about">Log in</router-link>
+            <router-link to="/about">Sign up</router-link>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbar"
+};
+</script>
