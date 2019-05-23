3e4f1b324147d7b0f8fcf7f8d88fd855a4555de5
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-05-22 20:31:49 -0400

    signup page started

--- client/src/views/SignUp.vue
+++ client/src/views/SignUp.vue
@@ -1,11 +1,79 @@
 <template>
-  <div class="signup">
-    <h1>This is a signup page</h1>
+  <!-- <body class="bg-dark"> -->
+  <div class="container">
+    <div class="card card-register mx-auto mt-5">
+      <div class="card-header">Register an Account</div>
+      <div class="card-body">
+        <form>
+          <div class="form-group">
+            <div class="form-row">
+              <div class="col-md-12">
+                <div class="form-label-group">
+                  <input
+                    type="text"
+                    id="firstLastName"
+                    class="form-control"
+                    placeholder="First and Last name"
+                    required="required"
+                    autofocus="autofocus"
+                  >
+                  <label for="firstLastName">First and Last name</label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="form-label-group">
+              <input
+                type="email"
+                id="inputEmail"
+                class="form-control"
+                placeholder="Email address"
+                required="required"
+              >
+              <label for="inputEmail">Email address</label>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="form-row">
+              <div class="col-md-6">
+                <div class="form-label-group">
+                  <input
+                    type="password"
+                    id="inputPassword"
+                    class="form-control"
+                    placeholder="Password"
+                    required="required"
+                  >
+                  <label for="inputPassword">Password</label>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-label-group">
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    class="form-control"
+                    placeholder="Confirm password"
+                    required="required"
+                  >
+                  <label for="confirmPassword">Confirm password</label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a class="btn btn-danger" href="login.html">Register</a>
+        </form>
+        <div class="text-center">
+          <a class="d-block small mt-3" href="Login.vue">Login Page</a>
+        </div>
+      </div>
+    </div>
   </div>
+  <!-- </body> -->
 </template>
 <script>
 export default {
-  name: "signup",
-
+  name: "signup"
 };
 </script>
