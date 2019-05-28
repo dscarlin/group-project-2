fbd88cbdf9751779a998c781207c35fab31a0575
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-05-28 15:28:26 -0400

    about page so far

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -16,22 +16,11 @@
               <h4>Minimum Viable Product:</h4>
               <p>This application will register a new user, allow existing users to log in, create a new group, search for members, allow users to add members or delete members, set your current availability, and send notifications for people currently available.</p>
             </div>
-            <div>
-              <h2 class="card-title text-center">
-                About:
-                <strong>team.slice("pizza")</strong>
-                <br>
-              </h2>
-              <div class="row">
-                <h1 class="text-center">pee</h1>
-              </div>
-              <div class="row">
-                <h1 class="text-center">poop</h1>
-              </div>
-              <div class="row">
-                <h1 class="text-center">vom</h1>
-              </div>
-            </div>
+            <h2 class="card-title text-center">
+              About Us:
+              <strong>team.slice("pizza")</strong>
+              <br>
+            </h2>
           </div>
         </div>
       </div>
