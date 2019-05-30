05fd06d860307fb68b4d62c97c321c73279368fd
Elvin <elveng@gmail.com>
2019-05-29 21:11:52 -0400

    updated eslint with routes for post, delete, amd get

--- db_routes/delete_db.js
+++ db_routes/delete_db.js
@@ -12,35 +12,49 @@ module.exports = (app, db) => {
     }).then( (res) => res ? res.status(200) : res.status(400) );
   });
 
-  //remove group
-    app.delete('/api/group/:id', (req, res) => {
-        console.log('called delete on group')
-        let id = req.params.id
-        db.UserGroup.destroy({where: {GroupId : id}}).then(result => {
-            if (result){
-                console.log(result);
-                db.Group.destroy({where: { id: id }}).then(result => { 
-                    result ? res.status(200).send(true) : res.status(400).send(false);
-                })
-            }
-            else 
-                res.status(400).send(false);
+  // Route to remove group
+  app.delete("/api/group/:id", (req, res) => {
+    console.log("called delete on group");
 
-            
-        })
-    })
+    let id = req.params.id;
+    db.UserGroup.destroy({
+      where: {
+        GroupId : id
+      }
+    }).then( (result) => {
+      if (result) {
+        console.log(result);
 
-    //remove user from group
-    app.delete('/api/user/group/:userId/:groupId', (req, res) => {
-        console.log(req.params)
-        let UserId = req.params.userId
-        let GroupId = req.params.groupId
-        db.UserGroup.destroy({where: { UserId: UserId, GroupId: GroupId }})
-        .then((result => {
-            console.log(result)
-            result ? res.status(200).send(true) : res.status(400).send(false)
-        }))
-    })
+        db.Group.destroy({
+          where: {
+            id: id
+          }
+        }).then( (result) => {
+          result ? res.status(200).send(true) : res.status(400).send(false);
+        });
+      } else {
+        res.status(400).send(false);
+      }
+    }); // end then()
+  }); // end delete()
 
+  //remove user from group
+  app.delete("/api/user/group/:userId/:groupId", (req, res) => {
+    console.log(req.params);
 
-}
+    let UserId = req.params.userId;
+    let GroupId = req.params.groupId;
+
+    db.UserGroup.destroy({
+      where: {
+        UserId: UserId,
+        GroupId: GroupId
+      }
+    }).then( (result) => {
+      console.log(result);
+
+      result ? res.status(200).send(true) : res.status(400).send(false);
+    });
+  });
+
+}; // end export{}
--- db_routes/post_db.js
+++ db_routes/post_db.js
@@ -1,77 +1,79 @@
 
-module.exports = (LocalStrategy,passport,app,db,bcrypt) => {
-    const Op = db.Sequelize.Op;
-    const saltRounds = 8
+module.exports = (LocalStrategy, passport, app, db, bcrypt) => {
+  const Op = db.Sequelize.Op;
+  const saltRounds = 8;
 
-    
-    // passport.use(new LocalStrategy({
-    //     usernameField: "email",
-    //     passwordField: "password"
-    // }, function(username, password, done) {
-    //     console.log(username);
-    //     console.log(password);
-    //     return done(null, "done")
-    
-    // }));
+  // Route for user login
+  app.post("/login", passport.authenticate("local"), (req, res) => {
+    //console.log(req.body);
+    console.log(req.user.get());
+    console.log(req.isAuthenticated());
 
-    //login
-    app.post('/login',passport.authenticate("local"), (req, res) => {
-        console.log(req.body)
-        console.log(req.user.get())
-        console.log(req.isAuthenticated())
-        res.json(req.user.id)
+    res.json(req.user.id);
+  });
 
-        // res.send('')
-    })
+  // Route to create user
+  app.post("/api/user", (req, res) => {
+    console.log("called post on user");
 
-    //create user
-    app.post('/api/user',  (req, res) => {
-        console.log('called post on user')
-        let r = req.body;
-        bcrypt.hash(r.password, saltRounds, (err, hash) => {
-            let userInfo = { 
-                user_name: r.user_name,
-                email: r.email,
-                password: hash,
-                phone_number: r.phone_number
-            };
-            console.log(userInfo.password)
-            db.User.create(userInfo).then(result => res.json(result));
-        });
+    let r = req.body;
+    bcrypt.hash(r.password, saltRounds, (err, hash) => {
+      let userInfo = {
+        user_name: r.user_name,
+        email: r.email,
+        password: hash,
+        phone_number: r.phone_number
+      };
+
+      console.log(userInfo.password);
+      db.User.create(userInfo).then( (result) => res.json(result) );
     });
+  });
 
-    app.post('/api/group/:uid', (req, res) => {
-        console.log('called post on group')
+  // Route to create a group
+  app.post("/api/group/:uid", (req, res) => {
+    console.log("called post on group");
 
-        
-        let query = {name: req.body.groupName}
-        db.Group.create(query)
-        .then((result => {
-            console.log(result.id)
-            db.UserGroup.create({UserId: req.params.uid,GroupId: result.id})
-            .then(result => {res.json(result)});
-        }));
-    })
+    let query = {
+      name: req.body.groupName
+    };
 
-    app.post('/api/user/:groupId', (req, res) => {
-        console.log('called post on userGroup')
-        let groupId = req.params.groupId;
-        let userId = req.body.userId;
-        console.log(groupId)
-        console.log(userId)
-        db.UserGroup.create( {UserId: userId, GroupId: groupId})
-        .then((result => res.json(result)));
-    })
+    db.Group.create(query).then( (result) => {
+      console.log(result.id);
+      db.UserGroup.create({
+        UserId: req.params.uid,
+        GroupId: result.id
+      }).then( (result) => {
+        res.json(result);
+      });
+    });
+  });
 
-    app.post('/api/twilio',function(req,res){
-        let body = req.body.message
-        console.log('twilio post')
-        // twilio.messages.create({
-        //   body: body,
-        //   from: trialNumber,
-        //   to: '+19193682008'
-        //   }).then(message => res.json(message.sid))
-      })
+  // Route to create user-group
+  app.post("/api/user/:groupId", (req, res) => {
+    console.log("called post on userGroup");
 
+    let groupId = req.params.groupId;
+    let userId = req.body.userId;
+    console.log(groupId);
+    console.log(userId);
+    db.UserGroup.create({
+      UserId: userId,
+      GroupId: groupId
+    }).then( (result) => {
+      res.json(result);
+    });
+  });
 
-}
+  // Route for twilio messaging
+  app.post("/api/twilio", (req, res) => {
+    let body = req.body.message;
+    console.log("twilio post");
+    // twilio.messages.create({
+    //   body: body,
+    //   from: trialNumber,
+    //   to: '+19193682008'
+    //   }).then(message => res.json(message.sid))
+  });
+
+}; // end export{}
