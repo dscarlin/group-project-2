423d251d1dd77927af4632128996953a2fc95957
Elvin <elveng@gmail.com>
2019-05-29 21:19:15 -0400

    updated eslint with db model files

--- models/UserGroup.js
+++ models/UserGroup.js
@@ -1,10 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
-    let UserGroup = sequelize.define("UserGroup", {});
-    UserGroup.associate = (models) => {
-      UserGroup.belongsTo(models.User);
-      UserGroup.belongsTo(models.Group);
-     
-    };  
-   
-    return UserGroup;
-}
+  let UserGroup = sequelize.define("UserGroup", { });
+
+  UserGroup.associate = (models) => {
+    UserGroup.belongsTo(models.User);
+    UserGroup.belongsTo(models.Group);
+  };
+
+  return UserGroup;
+
+}; // end export{}
