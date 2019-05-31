6dc012c19509766dd5340650cc0a734e00135ba6
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-05-31 18:24:16 -0400

    about page almost done added photos

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,31 +1,254 @@
 <template>
-<div class="about">
-  <body class="about-bg">
+<div class="aboutUs">
+  <body class="background">
     <div class="container">
       <div class="row">
         <div class="col-sm-10 col-md-10 col-lg-10 mx-auto">
-          <div class="card card-about my-5">
-            <div class="card-body">
-              <h1 class="card-title text-center">
+          <div class="aboutUs my-5">
+            <div class="jumbotron">
+              <h1 class="text-center">
                 <!-- logo here -->
                 <strong>REc(=)nnect</strong>
               </h1>
 
               <h4>Project Description:</h4>
-              <p>REc(=)nnect is for people to create groups and notify that they are available to catch up on the phone for a specified period of time. In a modern world of text-only communication with ambiguity of meanings and no assurance of a response, wouldn't it be nice to know who you could call on the phone without needing to know their schedule or feeling like you are going to bother them? You want to catch up, but you don't want to spend 20 minutes calling five or six people to see if they are free to talk. We have got you covered!</p>
+              <p>
+                REc(=)nnect is for people to create groups and notify that they are available to catch up on the
+                phone for a specified period of time. In a modern world of text-only communication with ambiguity of
+                meanings and no assurance of a response, wouldn't it be nice to know who you could call on the phone
+                without needing to know their schedule or feeling like you are going to bother them? You want to catch
+                up, but you don't want to spend 20 minutes calling five or six people to see if they are free to talk.
+                We have got you covered!
+              </p>
               <h4>Minimum Viable Product:</h4>
-              <p>This application will register a new user, allow existing users to log in, create a new group, search for members, allow users to add members or delete members, set your current availability, and send notifications for people currently available.</p>
-            </div>
-            <div>
-              <h2 class="card-title text-center">
-                About Us:
-                <strong>team.slice("pizza")</strong>
-                <br>
-              </h2>
+              <p>
+                This application will register a new user, allow existing users to log in, create a new group, search
+                for members, allow users to add members or delete members, set your current availability, and send
+                notifications for people currently available.
+              </p>
             </div>
           </div>
         </div>
       </div>
+      <!-- <div class="container"> -->
+      <div class="content">
+        <section id="team" class="team-area">
+          <!-- <div class="content"> -->
+          <div class="row justify-content-center">
+            <div class="col-lg-6">
+              <!-- <div class="section-title text-center"> -->
+              <h3 class="title text-center">
+                About Us:
+                <strong>team.slice("pizza")</strong>
+              </h3>
+              <!-- </div> -->
+              <!-- section title -->
+            </div>
+          </div>
+          <!-- row -->
+
+          <div class="row justify-content-center">
+            <div class="card">
+              <div class="card__side card__side--front">
+                <!-- Front Content -->
+                <div class="card__cont">
+                  <span class="blue">alert</span>
+                  <span>
+                    (
+                    <span class="green">'Hello World!'</span>)
+                  </span>
+                </div>
+              </div>
+              <div class="card__side card__side--back">
+                <!-- Back Content -->
+                <div class="card__cta">
+                  <p>
+                    <span class="purple">const</span> aboutMe
+                    <span class="cyan">=</span> {
+                    <br>
+                    <span class="space red">name</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'laura pinto'</span>,
+                    <br>
+                    <span class="space red">email</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'lauraalpinto@gmail.com</span>',
+                    <br>
+                    <span class="space red">position</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'front-end developer'</span>,
+                    <br>
+                    <span class="space red">website</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'lauraalpinto.github.io'</span>
+                    <br>};
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="card card-about">
+              <div class="card__side card__side--front">
+                <!-- Front Content -->
+                <div class="card__cont">
+                  <span class="blue">alert</span>
+                  <span>
+                    (
+                    <span class="green">'Hello World!'</span>)
+                  </span>
+                </div>
+              </div>
+              <div class="card__side card__side--back">
+                <!-- Back Content -->
+                <div class="card__cta">
+                  <p>
+                    <span class="purple">const</span> aboutMe
+                    <span class="cyan">=</span> {
+                    <br>
+                    <span class="space red">name</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'laura pinto'</span>,
+                    <br>
+                    <span class="space red">email</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'lauraalpinto@gmail.com</span>',
+                    <br>
+                    <span class="space red">position</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'front-end developer'</span>,
+                    <br>
+                    <span class="space red">website</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'lauraalpinto.github.io'</span>
+                    <br>};
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="card card-about">
+              <div class="card__side card__side--front">
+                <!-- Front Content -->
+                <div class="card__cont">
+                  <span class="blue">alert</span>
+                  <span>
+                    (
+                    <span class="green">'Hello World!'</span>)
+                  </span>
+                </div>
+              </div>
+              <div class="card__side card__side--back">
+                <!-- Back Content -->
+                <div class="card__cta">
+                  <p>
+                    <span class="purple">const</span> aboutMe
+                    <span class="cyan">=</span> {
+                    <br>
+                    <span class="space red">name</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'laura pinto'</span>,
+                    <br>
+                    <span class="space red">email</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'lauraalpinto@gmail.com</span>',
+                    <br>
+                    <span class="space red">position</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'front-end developer'</span>,
+                    <br>
+                    <span class="space red">website</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'lauraalpinto.github.io'</span>
+                    <br>};
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="card card-about">
+              <div class="card__side card__side--front">
+                <!-- Front Content -->
+                <div class="card__cont">
+                  <span class="blue">alert</span>
+                  <span>
+                    (
+                    <span class="green">'Hello World!'</span>)
+                  </span>
+                </div>
+              </div>
+              <div class="card__side card__side--back">
+                <!-- Back Content -->
+                <div class="card__cta">
+                  <p>
+                    <span class="purple">const</span> aboutMe
+                    <span class="cyan">=</span> {
+                    <br>
+                    <span class="space red">name</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'laura pinto'</span>,
+                    <br>
+                    <span class="space red">email</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'lauraalpinto@gmail.com</span>',
+                    <br>
+                    <span class="space red">position</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'front-end developer'</span>,
+                    <br>
+                    <span class="space red">website</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'lauraalpinto.github.io'</span>
+                    <br>};
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="card card-about">
+              <div class="card__side card__side--front">
+                <!-- Front Content -->
+                <div class="card__cont">
+                  <span class="blue">alert</span>
+                  <span>
+                    (
+                    <span class="green">'Hello World!'</span>)
+                  </span>
+                </div>
+              </div>
+              <div class="card__side card__side--back">
+                <!-- Back Content -->
+                <div class="card__cta">
+                  <p>
+                    <span class="purple">const</span> aboutMe
+                    <span class="cyan">=</span> {
+                    <br>
+                    <span class="space red">name</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'laura pinto'</span>,
+                    <br>
+                    <span class="space red">email</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'lauraalpinto@gmail.com</span>',
+                    <br>
+                    <span class="space red">position</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'front-end developer'</span>,
+                    <br>
+                    <span class="space red">website</span>
+                    <span class="cyan">:</span>
+                    <span class="green">'lauraalpinto.github.io'</span>
+                    <br>};
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- row -->
+
+          <!-- container -->
+        </section>
+      </div>
+      <!-- </div> -->
     </div>
   </body>
 </div>
@@ -41,27 +264,150 @@ export default {
   --input-padding-y: 0.75rem;
 }
 
-.about-bg {
+body {
   background: #00a799;
   background: linear-gradient(to right, #00a799, #b7e3e4);
-  height: 100vh;
 }
-.card-title,
-h2,
-h1 {
+
+h2 {
   margin-bottom: 2rem;
   font-weight: 300;
   font-size: 1.5rem;
   font-family: "Quicksand", sans-serif;
 }
-// h1 {
-//   font-size: 2rem;
-// }
+
+h1 {
+  margin-bottom: 2rem;
+  font-weight: 300;
+  font-family: "Quicksand", sans-serif;
+  font-size: 2rem;
+}
+
 p,
 h4 {
   font-size: 0.85rem;
 }
-.card-body {
-  padding: 2rem;
+
+// img {
+//   max-height: 100px;
+//   max-width: 100px;
+// }
+
+* {
+  font-family: Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console",
+    "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono",
+    "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier,
+    monospace;
+}
+
+$blue: #29b6f6;
+$green: #9ccc65;
+$purple: #ba68c8;
+$orange: #f57c00;
+$red: #ef5350;
+$cyan: #4dd0e1;
+
+@mixin text-color($color) {
+  color: $color;
+}
+
+.blue {
+  @include text-color($blue);
+}
+
+.green {
+  @include text-color($green);
+}
+
+.purple {
+  @include text-color($purple);
+}
+
+.cyan {
+  @include text-color($cyan);
+}
+
+.red {
+  @include text-color($red);
+}
+
+.content {
+  height: 100vh;
+  @include text-color(white);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card {
+  //FUNCTIONALITY
+  margin: 50px;
+  perspective: 150rem;
+  height: 10rem;
+  width: 10rem;
+
+  position: relative;
+
+  &__side {
+    height: 15rem;
+    transition: all 0.8s ease;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    margin: auto;
+    width: 15rem;
+
+    backface-visibility: hidden;
+    border-radius: 3px;
+    overflow: hidden;
+    box-shadow: 0 1.5rem 4rem rgba(black, 0.4);
+
+    &--front {
+      background-color: #1c1c1c;
+    }
+
+    &--back {
+      transform: rotateY(180deg);
+
+      background-color: #1c1c1c;
+    }
+  }
+
+  &:hover &__side--front {
+    transform: rotateY(-180deg);
+  }
+
+  &:hover &__side--back {
+    transform: rotateY(0deg);
+  }
+
+  //FRONT SIDE STYLING
+  &__cont {
+    height: 15rem;
+    background-color: #1c1c1c;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &__cta {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 90%;
+    @include text-color(white);
+
+    p {
+      margin-left: 1rem;
+
+      & > .space {
+        margin-left: 2rem;
+      }
+    }
+  }
 }
 </style>
