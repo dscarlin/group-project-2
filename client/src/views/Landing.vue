8c3a67ede950fe79b5fda06f13414c9ad2792ddf
lgfahys <lindsey@codeasite.com>
2019-06-01 14:09:19 -0400

    responsive home page carousel

--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -1,50 +1,58 @@
 <template>
-<div class="main">
-  <header>
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner" role="listbox">
-      <!-- Slide One - Set the background image for this slide in the line below -->
-      <div class="carousel-item active" :style="{ backgroundImage: 'url(' + require('../../public/images/upload_images/phone_talk.jpg') + ')'}">
-      <!-- :style="{ backgroundImage: 'url(' + require('@/assets/images/cards.jpg') + ')' }"> -->
-        <div class="carousel-caption d-none d-md-block">
-          <h3 class="display-4">First Slide</h3>
-          <p class="lead">This is a description for the first slide.</p>
+  <div class="main">
+    <header>
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+          <!-- Slide One - Set the background image for this slide in the line below -->
+          <div
+            class="carousel-item active"
+            :style="{ backgroundImage: 'url(' + require('../../public/images/upload_images/phone_talk.jpg') + ')'}"
+          ></div>
+          <!-- Slide Two - Set the background image for this slide in the line below -->
+          <div
+            class="carousel-item"
+            :style="{ backgroundImage: 'url(' + require('../../public/images/upload_images/phone-talk2.jpeg') + ')'}"
+          ></div>
+          <!-- Slide Three - Set the background image for this slide in the line below -->
+          <div
+            class="carousel-item"
+            :style="{ backgroundImage: 'url(' + require('../../public/images/upload_images/phone-talk3.jpeg') + ')'}"
+          ></div>
         </div>
-      </div>
-      <!-- Slide Two - Set the background image for this slide in the line below -->
-      <div class="carousel-item" :style="{ backgroundImage: 'url(' + require('../../public/images/upload_images/phone_talk4.jpg') + ')'}">
-        <div class="carousel-caption d-none d-md-block">
-          <h3 class="display-4">Second Slide</h3>
-          <p class="lead">This is a description for the second slide.</p>
-        </div>
-      </div>
-      <!-- Slide Three - Set the background image for this slide in the line below -->
-      <div class="carousel-item" :style="{ backgroundImage: 'url(' + require('../../public/images/upload_images/phone_talk5.jpg') + ')'}">
-        <div class="carousel-caption d-none d-md-block">
-          <h3 class="display-4">Third Slide</h3>
-          <p class="lead">This is a description for the third slide.</p>
-        </div>
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <a
+          class="carousel-control-prev"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="prev"
+        >
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <a
+          class="carousel-control-next"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="next"
+        >
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
+      </div>
+      <div class="card sign-up-card">
+        <div class="card-body">
+          <h5 class="card-title sign-up-card-title">Stop <br> Playing  <br> Phone  <br> Tag</h5>
+          <button class="btn sign-up-btn">
+            <router-link to="/signup" class="sign-up-router">Sign up for free</router-link>
+          </button>
+        </div>
+      </div>
+    </header>
   </div>
-</header>
-<div>
-  <p class="two">Some text.</p>
-</div>
-</div>
 </template>
 
 <script>
@@ -59,17 +67,13 @@ export default {
   data: function() {
     return {
       range: 0,
-      msg: {
-
-      },
+      msg: {},
       notification: "Steven Wright for the next 45 minutes"
-    }
+    };
   },
   created: function() {
     // let message = this.notification
     // this.notifyMe(message);
-    
-   
   },
   methods: {
     logMeOut: function() {
@@ -83,12 +87,8 @@ export default {
 </script>
 
 <style lang="scss">
-.landing {
-  background-image: url("../../public/images/upload_images/phone_talk.jpg");
-  background-size: cover;
-  // height: calc(100vh - $headerHeight);
-  //  background: linear-gradient(to right, #00a799, #b7e3e4);
-  height: 100vh;
+.carousel-item {
+  height: 65vh;
   min-height: 350px;
   background: no-repeat center center scroll;
   -webkit-background-size: cover;
@@ -96,4 +96,81 @@ export default {
   -o-background-size: cover;
   background-size: cover;
 }
+
+.sign-up-card-title {
+  color: black;
+  font-size: xx-large;
+  font-weight: 900;
+}
+
+.sign-up-card {
+  float: left;
+  position: absolute;
+  left: 65%;
+  top: 25%;
+  width: 15em;
+  border: none;
+  // box-shadow: 10px 10px 5px grey;
+  box-shadow: 10px 7px 17px 15px rgba(220, 220, 220, 0.35);
+  // height: 22em;
+  border-radius: 0;
+  text-align: left;
+}
+
+.sign-up-btn {
+  background-color: #ff585b;
+  color: white;
+  border-radius: 0;
+}
+
+.sign-up-btn {
+  background-color: #ff585b;
+  color: white;
+}
+
+.sign-up-router {
+  color: white;
+}
+
+.sign-up-router:hover {
+  color: black;
+  text-decoration: none;
+}
+
+@media (max-width: 930px) {
+  .sign-up-card {
+   left: 60%;
+  }
+}
+
+@media (max-width: 780px) {
+  .sign-up-card {
+   left: 50%;
+  }
+}
+
+@media (max-width: 600px) {
+  .sign-up-card {
+   left: 40%;
+  }
+}
+
+@media (max-width: 485px) {
+  .sign-up-card {
+   left: 30%;
+  }
+}
+
+@media (max-width: 400px) {
+  .sign-up-card {
+   left: 20%;
+  }
+}
+
+@media (max-width: 350px) {
+  .sign-up-card {
+   left: 17%;
+  }
+}
+
 </style>
